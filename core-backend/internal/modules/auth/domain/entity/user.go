ae6c98fb41836727c211788fccb611747dfba54e
Johna210 <solomonjohna21@gmail.com>
2026-03-06 00:32:10 +0300

    feat: test ci 3

--- core-backend/internal/modules/auth/domain/entity/user.go
+++ core-backend/internal/modules/auth/domain/entity/user.go
@@ -1,7 +1,13 @@
 package entity
 
-import "github.com/Final-Year-Project-G22/backend/core/internal/shared/model"
+import (
+	"github.com/Final-Year-Project-G22/backend/core/internal/shared/model"
+	"github.com/google/uuid"
+)
 
 type User struct {
 	model.BaseModel `gorm:"embedded"`
+
+	AccountId uuid.UUID `gorm:"type:uuid;not null;uniqueIndex"`
+	Account   Account   `gorm:"foreignKey:AccountId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
