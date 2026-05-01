c880c5b349494cf13ef94be27c7ca9c2864ecded
Johna210 <solomonjohna21@gmail.com>
2026-05-01 23:02:17 +0300

    fix(core): updated current iam notification setup

--- core-backend/internal/modules/iam/domain/event/events.go
+++ core-backend/internal/modules/iam/domain/event/events.go
@@ -3,5 +3,6 @@ package event
 const (
 	AccountRegistered     = "account.registered"
 	UserEmailOTPRequested = "user.email_otp_requested"
+	AccountAlert          = "account.alert"
 	AdminCreated          = "admin.created"
 )
