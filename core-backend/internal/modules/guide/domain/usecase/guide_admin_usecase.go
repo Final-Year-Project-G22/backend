4f55ca3cd3b93f455a50998266412fe8e0fdc07d
Johna210 <solomonjohna21@gmail.com>
2026-05-13 00:57:08 +0300

    fix(core): guide translation fixes

--- core-backend/internal/modules/guide/domain/usecase/guide_admin_usecase.go
+++ core-backend/internal/modules/guide/domain/usecase/guide_admin_usecase.go
@@ -11,16 +11,16 @@ import (
 )
 
 type GuideManagementUseCase interface {
-	ListGuides(ctx context.Context, q query.QueryOptions) (sharedrepo.PaginatedResult[entity.Guide], error)
+	ListGuides(ctx context.Context, q query.QueryOptions, locale constants.Locale) (sharedrepo.PaginatedResult[entity.Guide], error)
 	GetGuideDetail(ctx context.Context, guideID uuid.UUID, locale constants.Locale) (*entity.Guide, error)
-	ListGuideSteps(ctx context.Context, guideID uuid.UUID, q query.QueryOptions) (sharedrepo.PaginatedResult[entity.GuideStep], error)
+	ListGuideSteps(ctx context.Context, guideID uuid.UUID, q query.QueryOptions, locale constants.Locale) (sharedrepo.PaginatedResult[entity.GuideStep], error)
 
 	CreateGuide(ctx context.Context, input CreateGuideInput) (*entity.Guide, error)
 	UpdateGuide(ctx context.Context, id uuid.UUID, input UpdateGuideInput) error
 	DeleteGuide(ctx context.Context, id uuid.UUID) error
 	AddGuideCondition(ctx context.Context, guideID uuid.UUID, cond ConditionInput) error
 	RemoveGuideCondition(ctx context.Context, condID uuid.UUID) error
-	SetGuideTranslations(ctx context.Context, guideID uuid.UUID, translations []TranslationInput) error
+	SetGuideTranslations(ctx context.Context, guideID uuid.UUID, translations []TranslationInput, merge bool) error
 
 	CreateStep(ctx context.Context, input CreateStepInput) (*entity.GuideStep, error)
 	UpdateStep(ctx context.Context, id uuid.UUID, input UpdateStepInput) error
@@ -30,7 +30,7 @@ type GuideManagementUseCase interface {
 	RemoveStepCondition(ctx context.Context, condID uuid.UUID) error
 	AddStepDependency(ctx context.Context, stepID, requiredStepID uuid.UUID, depType entity.DependencyType) error
 	RemoveStepDependency(ctx context.Context, depID uuid.UUID) error
-	SetStepTranslations(ctx context.Context, stepID uuid.UUID, translations []StepTranslationInput) error
+	SetStepTranslations(ctx context.Context, stepID uuid.UUID, translations []StepTranslationInput, merge bool) error
 
 	GetStepVersions(ctx context.Context, stepID uuid.UUID, q query.QueryOptions) ([]*entity.GuideStepVersion, error)
 	RevertStepToVersion(ctx context.Context, stepID uuid.UUID, version int) error
