1b13c071edc0462853312200b3c2aa3fafea37ce
Johna210 <solomonjohna21@gmail.com>
2026-05-13 13:09:43 +0300

    fix(core): uuidArray type

--- core-backend/internal/modules/guide/domain/entity/guide.go
+++ core-backend/internal/modules/guide/domain/entity/guide.go
@@ -1,14 +1,11 @@
 package entity
 
-import (
-	"github.com/Final-Year-Project-G22/backend/core/internal/shared/model"
-	"github.com/google/uuid"
-)
+import "github.com/Final-Year-Project-G22/backend/core/internal/shared/model"
 
 type Guide struct {
 	model.BaseModel `gorm:"embedded"`
-	SectorIDs       []uuid.UUID        `gorm:"type:uuid[];index:idx_guides_sector_ids,using:gin"`
-	TagIDs          []uuid.UUID        `gorm:"type:uuid[];index:idx_guides_tag_ids,using:gin"`
+	SectorIDs       model.UUIDArray    `gorm:"type:uuid[];index:idx_guides_sector_ids,using:gin"`
+	TagIDs          model.UUIDArray    `gorm:"type:uuid[];index:idx_guides_tag_ids,using:gin"`
 	Slug            string             `gorm:"type:varchar(200);not null;uniqueIndex:idx_guides_slug"`
 	Icon            *string            `gorm:"type:varchar(100)"`
 	SortOrder       int                `gorm:"not null;default:0"`
